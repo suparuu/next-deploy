4a62e0759ab7ddf41dd222990ed8f9a2e4c8556b
suparuu <kkwoo97@naver.com>
2023-02-23 18:02:25 +0900

    18:02

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+/* const nextConfig = {
+  reactStrictMode: true,
+} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/{https://suparuu.github.io/next-deploy/}",
+  trailingSlash: true
 }
 
 module.exports = nextConfig
